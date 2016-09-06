f82faca20bcc9d1d00e1168c0d559893df56dd50
glingo <cailly.florian.apple@gmail.com>
2016-09-06 16:22:07 +0200

    cellrenderer

--- src/fr/afpa/librairie/model/list/AuteurListCellRenderer.java
+++ src/fr/afpa/librairie/model/list/AuteurListCellRenderer.java
@@ -5,15 +5,17 @@ import fr.afpa.librairie.data.bean.Auteur;
 import java.awt.Component;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.ListCellRenderer;
 
 
-public class AuteurListCellRenderer extends JLabel implements ListCellRendered<Auteur> {
+public class AuteurListCellRenderer extends JLabel implements ListCellRenderer<Auteur> {
     /**
      *
      */
     private static final long serialVersionUID = -1614367901813214864L;
     //identifiant unique. cf : deserialize, serialize.
     
+    @Override
     public Component getListCellRendererComponent(JList<? extends Auteur> list, Auteur value, int index, boolean isSelected, boolean cellHasFocus) {
         String toString = toString(value);
         setText(toString);
