f5634033f2185e13972ec6cc43f7b104507f52c5
Marine <marineg0108@gmail.com>
2016-09-08 12:02:05 +0200

    test JFormattedText pour la reduction

--- src/fr/afpa/librairie/controller/PromotionController.java
+++ src/fr/afpa/librairie/controller/PromotionController.java
@@ -2,13 +2,16 @@
 package fr.afpa.librairie.controller;
 
 import fr.afpa.librairie.data.bean.Promotion;
+import fr.afpa.librairie.data.exception.DAOException;
 import fr.afpa.librairie.model.list.ListAdapterListModel;
 import fr.afpa.librairie.view.MainFrame;
 import fr.afpa.librairie.view.admin.CreatePromotionPanel;
 import fr.afpa.librairie.view.admin.PromotionAdminPanel;
 import java.awt.event.ActionEvent;
 import java.sql.Date;
+import java.text.NumberFormat;
 import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class PromotionController extends Controller {
@@ -60,7 +63,9 @@ public class PromotionController extends Controller {
         
         JFormattedTextField fieldDateDebut = this.createPanel.getForm().getField("Date de début");
         JFormattedTextField fieldDateFin = this.createPanel.getForm().getField("Date de fin");
-        JTextField fieldReduction = this.createPanel.getForm().getField("Réduction");
+        JFormattedTextField fieldReduction = new JFormattedTextField(NumberFormat.getPercentInstance());
+        //formatter le champs texte pour acceuillir un float et l'afficher en pourcentage.
+        fieldReduction = this.createPanel.getForm().getField("Reduction");
         JTextField fieldImage = this.createPanel.getForm().getField("Image");
         JTextField fieldDescription = this.createPanel.getForm().getField("Description");
         JTextField fieldCommentaire = this.createPanel.getForm().getField("Commentaire");
@@ -69,29 +74,34 @@ public class PromotionController extends Controller {
         
         Date dateDebut = (Date) fieldDateDebut.getValue();
         Date dateFin = (Date) fieldDateFin.getValue();
-        String reduction = fieldReduction.getText();
+        Float reduction = (Float) fieldReduction.getValue();
         String image = fieldImage.getText();
         String description = fieldDescription.getText();
         String commentaire = fieldCommentaire.getText();
         
         
-        Promotion promotion = new Promotion();
-        //**********************************REPRISE ICI********************************
-
-//        promotion.setNumero(numero);
-//        commande.setDateCommande(new java.sql.Date(dateCommande.getTime()));
-
         
-//        try{
-//            getDaoFactory().getCommandeDAO().save(commande);
-//        } catch(DAOException ex){
-//            JOptionPane.showMessageDialog(this.frame, ex.getMessage(),
-//                    "Une erreur est survenue !", JOptionPane.ERROR_MESSAGE);
-//        }
-//        
+        
+        Promotion promotion = new Promotion();
+        
+        promotion.setDateDebut(new java.sql.Date(dateDebut.getTime()));
+        promotion.setDateFin(new java.sql.Date(dateFin.getTime()));
+        promotion.setReduction(reduction);
+        promotion.setImage(image);
+        promotion.setDescription(description);
+        promotion.setCommentaire(commentaire);
+   
+        try{
+            getDaoFactory().getPromotionDAO().save(promotion);
+        } catch(DAOException ex){
+            JOptionPane.showMessageDialog(this.frame, ex.getMessage(),
+                    "Une erreur est survenue !", JOptionPane.ERROR_MESSAGE);
+        }
+        
         this.createPanel.getForm().reset();
     
         listAction();
     }
+
     
 }
