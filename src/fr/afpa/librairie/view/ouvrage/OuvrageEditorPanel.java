29b7af95a5a0c69a2ae20a74a35f715b4b241ff8
Marine <marineg0108@gmail.com>
2016-09-15 11:03:48 +0200

    MAJ Ouvrage
    Mise a jour d'ouvragre fonctionnelle

--- src/fr/afpa/librairie/view/ouvrage/OuvrageEditorPanel.java
+++ src/fr/afpa/librairie/view/ouvrage/OuvrageEditorPanel.java
@@ -10,6 +10,8 @@ import javax.swing.GroupLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 
@@ -26,7 +28,8 @@ public class OuvrageEditorPanel extends EditorPanel<Ouvrage> {
     
     private JTextField titre;
     private JTextField sousTitre;
-    private JTextField resume;
+    private JTextArea resume;
+    private JScrollPane scrollPane;
     
     
     public OuvrageEditorPanel(ActionListener controller) {
@@ -39,44 +42,53 @@ public class OuvrageEditorPanel extends EditorPanel<Ouvrage> {
 
     @Override
     protected void initBody(JPanel body) {
-        titre = new JTextField();
-        sousTitre = new JTextField();
-        resume = new JDateField(false);
         
-        
+        scrollPane = new JScrollPane();
+         
         titreLabel = new JLabel("Titre :");
         sousTitreLabel = new JLabel("Sous-titre :");
         resumeLabel = new JLabel("Resume :");
+        
+        scrollPane.setViewportView(resume);
+        
+        titre = new JTextField();
+        sousTitre = new JTextField();
+        resume = new JTextArea();
         
         
         titre.setInputVerifier(new StrictInputVerifier());
         
         GroupLayout bodyPanelLayout = new GroupLayout(body);
         body.setLayout(bodyPanelLayout);
-
+        
         bodyPanelLayout.setHorizontalGroup(
             bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(bodyPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(bodyPanelLayout.createSequentialGroup()
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(GroupLayout.Alignment.TRAILING, bodyPanelLayout.createSequentialGroup()
                         .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                             .addGroup(bodyPanelLayout.createSequentialGroup()
                                 .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                    .addComponent(titreLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(sousTitreLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(resumeLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(13, 13, 13)
+                                    .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(titreLabel, GroupLayout.DEFAULT_SIZE, 89, Short.MAX_VALUE)
+                                        .addComponent(sousTitreLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    
+                                    .addComponent(resumeLabel))
                                 .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
                                     .addComponent(titre, GroupLayout.Alignment.LEADING)
                                     .addComponent(sousTitre, GroupLayout.Alignment.LEADING)
-                                    .addComponent(resume, GroupLayout.Alignment.LEADING))))
+                                    .addComponent(resume, GroupLayout.Alignment.LEADING)
+                                    .addComponent(scrollPane))))
                         .addGap(6, 6, 6))))
-        ));
-
+        );
+        
         bodyPanelLayout.setVerticalGroup(
             bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(bodyPanelLayout.createSequentialGroup()
+                .addGap(3, 3, 3)
                 .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
                     .addComponent(titreLabel)
                     .addComponent(titre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
@@ -87,9 +99,16 @@ public class OuvrageEditorPanel extends EditorPanel<Ouvrage> {
                 .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                     .addComponent(resume, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addComponent(resumeLabel))
-               
+                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(bodyPanelLayout.createSequentialGroup()
+                        .addGap(9, 9, 9)
+                        .addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                    .addGroup(bodyPanelLayout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(resumeLabel)))
                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED))
         );
+        
     }
 
     @Override
