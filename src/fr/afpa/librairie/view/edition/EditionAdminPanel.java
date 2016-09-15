eee6162ad1b3cf97bea45e3d120ac85ba21d706c
Marine <marineg0108@gmail.com>
2016-09-15 13:59:59 +0200

    EditionAdminPanel
    Rattachement du panel a ActivableFooterAdminPanel

--- src/fr/afpa/librairie/view/edition/EditionAdminPanel.java
+++ src/fr/afpa/librairie/view/edition/EditionAdminPanel.java
@@ -1,10 +1,12 @@
 package fr.afpa.librairie.view.edition;
 
+import fr.afpa.librairie.controller.EditionController;
 import fr.afpa.librairie.data.bean.Edition;
 import fr.afpa.librairie.model.table.EditionTableModel;
 import fr.afpa.librairie.view.panel.AdminPanel;
 import fr.afpa.librairie.view.panel.ListPanel;
 import fr.afpa.librairie.view.panel.TitlePanel;
+import fr.afpa.librairie.view.panel.footer.ActivableFooterAdminPanel;
 import fr.afpa.librairie.view.panel.footer.FooterAdminPanel;
 import java.awt.Component;
 import java.awt.event.ActionListener;
@@ -12,44 +14,13 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class EditionAdminPanel extends AdminPanel<Edition> {
-    
+
     private final static String TITLE = "Administration des editions.";
 
     public EditionAdminPanel(ActionListener controller) {
         super(new TitlePanel(TITLE), 
             new ListPanel<>(new EditionTableModel()), 
-            new FooterAdminPanel(controller));
+            new ActivableFooterAdminPanel(controller));
     }
-    
-    //methode pour dire ce que va comporter le footer/ 
-    private Component getFooter() {
-        JPanel footer = new JPanel();
-        
-        JButton createButton = new JButton("Creer");
-        JButton deactivateButton = new JButton("Désactiver");
-        JButton activateButton = new JButton("Activer");
-        JButton viewButton = new JButton("Voir");
-        JButton updateButton = new JButton("Mettre à jour");
-        
-        createButton.setActionCommand("create");
-//        createButton.addActionListener(controller);
-        
-        deactivateButton.setActionCommand("deactivate");
-//        deactivateButton.addActionListener(controller);
-        
-        activateButton.setActionCommand("activate");
-//        activateButton.addActionListener(controller);
-        
-
-        
-        footer.add(createButton);
-        footer.add(deactivateButton);
-        footer.add(activateButton);
-        footer.add(viewButton);
-        footer.add(updateButton);
-        footer.repaint();
-        return footer;
-    }
- 
-    
+  
 }
