c9f5ad7265f9154c13eac3efa88278153b0e25dd
Wint3rNuk3 <michaeldanielcarrasco@gmail.com>
2016-09-15 11:24:54 +0200

    CommandeEditorPanel part 1 (vrai)

--- src/fr/afpa/librairie/view/commande/CommandeEditorPanel.java
+++ src/fr/afpa/librairie/view/commande/CommandeEditorPanel.java
@@ -1,21 +1,24 @@
 package fr.afpa.librairie.view.commande;
 
 import fr.afpa.librairie.data.bean.Commande;
-import fr.afpa.librairie.data.bean.Rubrique;
+import fr.afpa.librairie.model.verifier.StrictInputVerifier;
 import fr.afpa.librairie.view.field.JDateField;
 import fr.afpa.librairie.view.panel.EditorPanel;
-import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.sql.Date;
 import javax.swing.GroupLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 
 public class CommandeEditorPanel extends EditorPanel<Commande> {
+    
+        private JLabel numeroLabel;
+        private JLabel dateCommandeLabel;
+        
+        private JTextField numero;
+        private JDateField dateCommande;
     
     public CommandeEditorPanel(ActionListener controller) {
         super(new Commande(), controller);
@@ -48,43 +51,71 @@ public class CommandeEditorPanel extends EditorPanel<Commande> {
     
     @Override
     protected void initBody(JPanel body) {
+        numero = new JTextField();
+        dateCommande = new JDateField(false);
         
+        numeroLabel = new JLabel("Numero :");
+        dateCommandeLabel = new JLabel("Date de commande :");
+        
+        numero.setInputVerifier(new StrictInputVerifier());
+        
+        GroupLayout bodyPanelLayout = new GroupLayout(body);
+        body.setLayout(bodyPanelLayout);
+
+        bodyPanelLayout.setHorizontalGroup(
+            bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(bodyPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(GroupLayout.Alignment.TRAILING, bodyPanelLayout.createSequentialGroup()
+                        .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(bodyPanelLayout.createSequentialGroup()
+                                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addComponent(numeroLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(dateCommandeLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(13, 13, 13)
+                                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                    .addComponent(numero, GroupLayout.Alignment.LEADING)
+                                    .addComponent(dateCommande, GroupLayout.Alignment.LEADING))))
+                        .addGap(6, 6, 6))))
+        );
+
+        bodyPanelLayout.setVerticalGroup(
+            bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(bodyPanelLayout.createSequentialGroup()
+                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addComponent(numeroLabel)
+                    .addComponent(numero, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(bodyPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(dateCommandeLabel)
+                    .addComponent(dateCommande, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED))
+        );
     }   
     
     @Override
     public void bindValues() {
         Commande commande = getBean();
-//        libelleTF.setText(rubrique.getLibelle());
-//        dateDebutTF.setValue(rubrique.getDateDebut());
-//        dateFinTF.setValue(rubrique.getDateFin());
-//        commentaireTF.setText(rubrique.getCommentaire());
+        numero.setText(commande.getNumero());
+        dateCommande.setValue(commande.getDateCommande());
     }
 
     @Override
     public void reset() {
-//        libelleTF.setText("");
-//        dateDebutTF.setValue(null);
-//        dateFinTF.setValue(null);
-//        commentaireTF.setText("");
+        numero.setText("");
+        dateCommande.setValue(null);
     }
 
     @Override
     public Commande constructBean() {
         Commande commande = getBean();
         
-//        rubrique.setLibelle(libelleTF.getText().trim());
-//        
-//        if(dateDebutTF.getValue() != null) {
-//            Date debut = new Date(dateDebutTF.getValue().getTime());
-//            rubrique.setDateDebut(debut);
-//        }
-//        
-//        if(dateFinTF.getValue() != null) {
-//            Date fin = new Date(dateFinTF.getValue().getTime());
-//            rubrique.setDateFin(fin);
-//        }
-//        
-//        rubrique.setCommentaire(commentaireTF.getText().trim());
+        
+        commande.setNumero(numero.getText());
+        
+        if(dateCommande.getValue() != null) {
+            commande.setDateCommande(new java.sql.Date(dateCommande.getValue().getTime()));
+        }
         
         return commande;
     }
